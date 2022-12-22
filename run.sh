5e6830bda08d4206a335b390728a0a96518bdb2a
mogita <me@mogita.com>
2022-12-22 14:20:50 +0800

    fix: add bundle exec for the correct context

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 set -e
 
 echo "---> db migration..."
-docker-compose run --rm web rake db:migrate && sudo
+docker-compose run --rm web bundle exec rake db:migrate
 
 echo "---> assets precompilation..."
-docker-compose run --rm web rake assets:precompile
+docker-compose run --rm web bundle exec rake assets:precompile
 
 echo "---> bringing up containers..."
 docker-compose up -d --build
