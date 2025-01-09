57dfa88856a8097e0ce1a251dac01d868fc22f44
mogita <me@mogita.com>
2025-01-09 14:16:21 +0800

    feat: update mastodon setup command

--- run.sh
+++ run.sh
@@ -25,8 +25,8 @@ chown -R 1000:1000 elasticsearch
 echo "---> db migration..."
 docker-compose run --rm web bundle exec rake db:migrate
 
-echo "---> assets precompilation..."
-docker-compose run --rm web bundle exec rake assets:precompile
+echo "---> running mastodon setup..."
+docker-compose run --rm web bundle exec rake mastodon:setup
 
 echo "---> bringing up containers..."
 docker-compose up -d --build --remove-orphans --force-recreate
